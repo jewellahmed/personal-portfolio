bbd535ed9618624713cddbb31f25c160da74ba4c
Saifullah Monsur Jewel <jewellahmed97@gmail.com>
2021-05-02 19:43:26 +0600

    second-commit

---
+++ src/components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqTXne1gK_n9REeIW_rzPZuYnvsEW3tg4",
+    authDomain: "saif-portfolio.firebaseapp.com",
+    projectId: "saif-portfolio",
+    storageBucket: "saif-portfolio.appspot.com",
+    messagingSenderId: "103161568886",
+    appId: "1:103161568886:web:9eb3adba10c5bd5a730957"
+  };
+  export default firebaseConfig;
